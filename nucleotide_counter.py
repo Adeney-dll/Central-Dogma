96662f351ce5d7b8d6e1cb9319884662eb3ea0c5
Adeney <philocity@proton.me>
2026-08-26 19:48:45 +0100

    N-counter-task-completed

---
+++ nucleotide_counter.py
@@ -0,0 +1,9 @@
+dna = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
+
+a_count = dna.count("A")
+g_count = dna.count("G")
+c_count = dna.count("C")
+t_count = dna.count("T")
+
+print(f"{a_count} {c_count} {g_count} {t_count}")
+assert f"{a_count} {c_count} {g_count} {t_count}" == "20 12 17 21"
