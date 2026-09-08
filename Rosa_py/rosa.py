8323f6a3cc2e2af2b375b86fd3f41499e3ff177b
Adeney <philocity@proton.me>
2026-09-08 17:22:19 +0100

    B

--- Rosa_py/rosa.py
+++ Rosa_py/rosa.py
@@ -2,7 +2,17 @@
 a = 973 ** 2
 b = 815 ** 2   # ** means race to power
 c = a + b
-print (c) # 1610954 """
+print (c) # 1610954 
 
-a =
+# Task 2, Lists
+tea_party = ['March Hare', 'Hatter', 'Dormouse', 'Alice']
+print (tea_party[3]) # making and calling items in a list 
+
+tea_party.append ('deeznuts') # adds an item to tthe end of the list """
+
+s = 'PfPn6rRnFZj3T3cAmLDl0cxR2P0CoturnixYSUTOVcaelebsqZla4Pqiw6ukyOs1COSwx2aJiyeBYPFRLF0PAfkpRoD26Z4n8uflEa9CqM9yJzFOnpJ1X3sY7tKS0UFVfRYjxBECDHxPLGAG39PgBpsN3d0Hdz4C7EoSk9xr6gpIUIdmUvrMzjKOWLDg.'
+word1 = s[27:34+1] 
+word2 = s[41:47+1]
+print (word1+ ' '+word2) # a string is technically a kind of list apparently
+
 
