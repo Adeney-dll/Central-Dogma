254d4de610a05d65bbdc9d08ce7b38dce69ce7f3
Adeney <philocity@proton.me>
2026-09-14 11:31:03 +0100

    U-1

---
+++ Rosa_py/list_task02.py
@@ -0,0 +1,10 @@
+# Task 2, Lists
+tea_party = ['March Hare', 'Hatter', 'Dormouse', 'Alice']
+print (tea_party[3]) # making and calling items in a list 
+
+tea_party.append ('deeznuts') # adds an item to tthe end of the list """
+
+s = 'PfPn6rRnFZj3T3cAmLDl0cxR2P0CoturnixYSUTOVcaelebsqZla4Pqiw6ukyOs1COSwx2aJiyeBYPFRLF0PAfkpRoD26Z4n8uflEa9CqM9yJzFOnpJ1X3sY7tKS0UFVfRYjxBECDHxPLGAG39PgBpsN3d0Hdz4C7EoSk9xr6gpIUIdmUvrMzjKOWLDg.'
+word1 = s[27:34+1] 
+word2 = s[41:47+1]
+print (word1+ ' '+word2) # a string is technically a kind of list apparently
--- Rosa_py/num_task01.py
+++ Rosa_py/num_task01.py
@@ -1,18 +1,8 @@
-"""# Python task 1, manipulating variables
+# Python task 1, manipulating variables
 a = 973 ** 2
 b = 815 ** 2   # ** means race to power
 c = a + b
 print (c) # 1610954 
 
-# Task 2, Lists
-tea_party = ['March Hare', 'Hatter', 'Dormouse', 'Alice']
-print (tea_party[3]) # making and calling items in a list 
-
-tea_party.append ('deeznuts') # adds an item to tthe end of the list """
-
-s = 'PfPn6rRnFZj3T3cAmLDl0cxR2P0CoturnixYSUTOVcaelebsqZla4Pqiw6ukyOs1COSwx2aJiyeBYPFRLF0PAfkpRoD26Z4n8uflEa9CqM9yJzFOnpJ1X3sY7tKS0UFVfRYjxBECDHxPLGAG39PgBpsN3d0Hdz4C7EoSk9xr6gpIUIdmUvrMzjKOWLDg.'
-word1 = s[27:34+1] 
-word2 = s[41:47+1]
-print (word1+ ' '+word2) # a string is technically a kind of list apparently
 
 
